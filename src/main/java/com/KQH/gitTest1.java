2da263afed12f7cd388ed2e961a7675d8624eba2
kqh <1728209057@qq.com>
2022-06-23 08:13:33 +0800

    second commit

--- src/main/java/com/KQH/gitTest1.java
+++ src/main/java/com/KQH/gitTest1.java
@@ -4,7 +4,6 @@ public class gitTest1 {
     public static void main(String[] args) {
         System.out.println("KQH -^_^-");
         System.out.println("KQH -^2^-");
-        System.out.println("KQH -^_^-");
 
     }
 
